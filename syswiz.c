b5d592c5866836407d31c1aada9387632d846024
Daniel Manesajian <manesajian@gmail.com>
2015-05-14 15:21:40 -0700

    Fixed a number of syntax issues. Added stub find_files().

--- syswiz.c
+++ syswiz.c
@@ -1,81 +1,98 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <string.h>
 
 #define MAX_PATH_LEN    2048
 
-typedef
-struct options_struct {
+struct options {
     int find_files_from_inode;
-    int inode_num;
+    int inode;
     int path_set;
     char path[MAX_PATH_LEN];
-} opt_t;
+};
 
-typedef
-struct output_struct {
+struct output {
 
-} out_t;
+};
 
 // Fills options structure, returns non-zero on error
-int parse_options(opt_t *opts, int argc, char **argv);
+int parse_options(struct options *opt, int argc, char **argv);
 
-void handle_output(opt_t *opts, out_t *outs);
+void handle_output(struct options *opt, struct output *out);
+
+int verify_path(char *path);
+
+struct output *find_files(struct options * opt);
 
 int main(int argc, char **argv)
 {
-    opt_t opts;
-    if (parse_options(&opts, argc, argv)) {
+    struct options opt;
+    struct output out;
+    if (parse_options(&opt, argc, argv)) {
         return 1;
     } 
 
-    if (opts.find_files_from_inode)
-        handle_output(&opts, find_files(&opts));
+    if (opt.find_files_from_inode)
+        handle_output(&opt, find_files(&opt));
 
     return 0;
 }
 
-int parse_options(opt_t *opts, int argc, char **argv)
+int parse_options(struct options *opt, int argc, char **argv)
 {
-    while ((o = getopt (argc, argv, "f:p:")) != -1) {
-        switch (o) {
+    char c;
+    while ((c = getopt (argc, argv, "f:i:p:")) != -1) {
+        switch (c) {
             case 'f':
-                opts->find_files_from_inode = 1;
+                opt->find_files_from_inode = 1;
+                break;
+            case 'i':
+                opt->inode = atoi(optarg);
                 break;
             case 'p':
-                opts->path_set = 1;
-                strncpy(opts->path, optarg, strlen(optarg))
+                opt->path_set = 1;
+                strncpy(opt->path, optarg, strlen(optarg));
                 break;
            default:
-                printf("Unrecognized arg: %s\n", o);
+                printf("Unrecognized arg: %c\n", c);
                 return 1;
        }
     }
 
-    if (opts->path_set) {
+    if (opt->path_set) {
+        printf("Verifying path ...");
         if (!verify_path(opt->path))
-            printf("Invalid path: %s\n", opts->path);
+            printf("Invalid path: %s\n", opt->path);
             return 1;
     }
 
     return 0;
 }
 
-void handle_output(opt_t *opts, out_t *outs)
+void handle_output(struct options *opt, struct output *out)
 {
-
+    printf("handle_output()\n");
 }
 
 int verify_path(char *path)
 {
-    DIR* dir = opendir("mydir");
-    if (dir) {
-        /* Directory exists. */
+    DIR *dir = opendir(path);
+    if (opendir(path)) {
+        // path exists
         closedir(dir);
     }
-    else if (ENOENT == errno) {
-        /* Directory does not exist. */
+    else if (errno == ENOENT) {
+        printf("Directory %s does not exist.", path);
     }
     else {
-        /* opendir() failed for some other reason. */
+        printf("Directory %s could not be accessed.", path);
     }
 }
 
-
+struct output *find_files(struct options * opt)
+{
+    DIR *dir = opendir(opt->path);
+}
