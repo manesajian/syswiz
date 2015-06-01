9efc565b1c04fc1c6233ed87c37737ab7ea6cb57
Daniel Manesajian <manesajian@gmail.com>
2015-06-01 12:13:22 -0700

    Cleaned up main() and basic unit tests.

--- llist.c
+++ llist.c
@@ -184,25 +184,44 @@ void del_llnode(llist *ll, llnode *node)
     free(node);
 }
 
+typedef struct test_st test_st;
+
+typedef struct test_st
+{
+    llnode hdr;
+    int val;
+} test_st;
+
 int main(int argc, char *argv[])
 {
     llist *ll = NULL;
     ll = malloc(sizeof(llist));
     memset(ll, 0, 0);
 
-    printf("Allocating and initializing list ...\n");
+    printf("Allocating list ...\n");
     ll = malloc(sizeof(llist));
     memset(ll, 0, 0);
 
-    printf("Attempting to initialize 100-element linked list ...\n");
+    printf("Initializing list ...\n");
 
     int i;
-    for (i = 0; i < 100; ++i) {
-        llnode *node = malloc(sizeof(llnode));
-        free(node);
+    for (i = 0; i < 5; ++i) {
+        test_st *node = malloc(sizeof(test_st));
+        node->val = i;
+        printf("\tAdding element val %i ...\n", node->val);
+        add_llnode(ll, (llnode *)node);
     }
 
-    free(ll);
+    printf("Freeing list ...\n");
+
+    while (ll->count) {
+        printf("\tFreeing element val %i ...\n", ((test_st *)(ll->head))->val);
+        del_llnode(ll, ll->head);
+    }
+
+    free_llist(ll);
+
+    printf("Completed.\n");
 
     return 0;
 }
