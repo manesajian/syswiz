8554fe3382a87e18d595ef050fb351b8d6982448
Daniel Manesajian <manesajian@gmail.com>
2015-06-04 22:49:38 -0700

    Fixed hash function to return % buckets. Including current debugging.

--- hashtable.c
+++ hashtable.c
@@ -44,21 +44,28 @@ unsigned long hash_key(htable *ht, char *str)
     while ((c = *str++))
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-    return hash;
+    return hash % ht->buckets;
 }
 
 void add_item(htable *ht, char *key, void *value)
 {
     unsigned long hash = hash_key(ht, key);
+    printf("MarkA: %lu\n", hash);
 
     llist *bucket = ht->keys[hash];
     if (bucket == NULL)
         bucket = ht->keys[hash] = get_llist();
+    printf("MarkB\n");
 
     htnode *node = malloc(sizeof(htnode) + strnlen(key, HT_MAX_KEY_LEN));
+    printf("MarkC\n");
     node->elem.data = value;
-    strncpy(node->key, key, HT_MAX_KEY_LEN);
+    printf("MarkD\n");
+
+    strncpy(&(node->key), key, HT_MAX_KEY_LEN);
+    printf("MarkE\n");
     node->key[HT_MAX_KEY_LEN] = 0;
+    printf("MarkF\n");
 
     add_llnode_head(bucket, (llnode *)node);
 }
@@ -73,7 +80,7 @@ void *get_item(htable *ht, char *key)
 
     llnode *node = bucket->head;
     while (node) {
-        if (!strncmp(key, ((htnode *)node)->key, HT_MAX_KEY_LEN))
+        if (!strncmp(key, *((htnode *)node)->key, HT_MAX_KEY_LEN))
             break;
         node = node->next;
     }
@@ -90,7 +97,7 @@ void del_item(htable *ht, char *key)
 
     llnode *node = bucket->head;
     while (node) {
-        if (!strncmp(key, ((htnode *)node)->key, HT_MAX_KEY_LEN))
+        if (!strncmp(key, *((htnode *)node)->key, HT_MAX_KEY_LEN))
             break;
         node = node->next;
     }
