921a5563892ce7209a0abb76ff649a89f6abd36e
Daniel Manesajian <manesajian@gmail.com>
2015-06-09 16:14:47 -0700

    Adding incomplete implementation.

---
+++ insertionsort.c
@@ -0,0 +1,14 @@
+llist *insertion_sort(llist *lst)
+{
+    llist *sorted = get_llist();
+
+    while (lst->count > 0) {
+        llnode *cur = pop_llist_head(llist *ll);
+
+        for (;;) {
+
+        }
+    }
+
+    return sorted;
+}
---
+++ insertionsort.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDED_INSERTIONSORT_H
+#define INCLUDED_INSERTIONSORT_H
+
+#include "linkedlist.h"
+
+llist *insertion_sort(llist *lst);
+
+#endif
