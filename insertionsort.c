4b76e3600ad302f600056d7cfcd7a55f4546730c
Daniel Manesajian <manesajian@gmail.com>
2015-06-09 20:52:25 -0700

    Committing an incomplete but compiling version. Currently mainly lacking compare function.

--- insertionsort.c
+++ insertionsort.c
@@ -1,27 +1,29 @@
+#include "insertionsort.h"
+
 /* TODO: need to pass in a compare function */
 
-llist *insertion_sort(llist *lst)
+llist *insertion_sort(llist *ll)
 {
     llist *sorted = get_llist();
 
 /* TODO: examine optimization which looks at both ends of the list to decide
    which end to start from. I.e., detects direction of possible orderedness. */
 
-    while (lst->count > 0) {
-        llnode *cur = pop_llist_head(llist *ll);
+    while (ll->count > 0) {
+        llnode *cur = pop_llist_head(ll);
 
         llnode *tmp = sorted->head;
-        while(tmp) {
-            if ((compare)(tmp, cur)) <= 0) {
-                add_llnode_before(sorted, tmp, cur);
-                break;
-            }
+        while (tmp) {
+            //if (((compare)(tmp, cur)) <= 0) {
+            //    add_llnode_before(sorted, tmp, cur);
+            //    break;
+            //}
 
             tmp = tmp->next;
         }
 
         if (tmp)
-            add_llnode_tail(cur);
+            add_llnode_tail(ll, cur);
     }
 
     return sorted;
