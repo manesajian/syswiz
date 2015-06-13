08c856e9b5dac5cc22b1d8ddc2a7a2c8fcc5a41e
Daniel Manesajian <manesajian@gmail.com>
2015-06-12 21:21:29 -0700

    Initial add of heapsort files.

---
+++ heapsort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include "heapsort.h"
+
+llist *heapsort(llist *ll)
+{
+    // Can't sort without a compare function.
+    if (ll->compare == NULL)
+        return ll;
+
+
+
+
+
+/*
+    llist *sorted = get_llist();
+
+    while (ll->count > 0) {
+        llnode *cur = pop_llist_head(ll);
+
+        llnode *tmp = sorted->head;
+        while (tmp) {
+            if (ll->compare(tmp, cur) >= 0) {
+                add_llnode_before(sorted, tmp, cur);
+                break;
+            }
+
+            tmp = tmp->next;
+        }
+
+        // tmp will be NULL if end of sorted list was reached
+        if (!tmp)
+            add_llnode_tail(sorted, cur);
+    }
+
+    return sorted;
+
+*/
+}
---
+++ heapsort.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDED_HEAPSORT_H
+#define INCLUDED_HEAPSORT_H
+
+#include "linkedlist.h"
+
+llist *heapsort(llist *lst);
+
+#endif
