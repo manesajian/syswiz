581af6333cfa8eba49db89e005b150453b4f8815
Daniel Manesajian <manesajian@gmail.com>
2015-06-16 11:10:30 -0700

    Added bst iterator.

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -5,10 +5,38 @@ class BSTNode(TreeNode):
         TreeNode.__init__(self)
 
         self.key = key
+        self.leftChild = None
+        self.rightChild = None
 
 class BinarySearchTree(BinaryTree):
     def __init__(self):
         BinaryTree.__init__(self)
+
+    def __iter__(self):
+        self.__node = self.root
+        if self.__node:
+            if self.__node.leftChild:
+                for elem in self.__node.leftChild:
+                    yield elem
+            yield self.__node.key
+            if self.__node.rightChild()
+                for elem in self.__node.rightChild:
+                    yield elem
+
+    def __next__(self):
+        returnObj = self.__next
+
+        if returnObj is None:
+                raise StopIteration
+
+        if returnObj.leftChild:
+            self.__next = returnObj.leftChild
+        elif returnObj.rightChild:
+            self.__next = returnObj.rightChild
+        else:
+            self.__next = None
+
+        return returnObj
 
     def insert_node(self, node):
         pass
