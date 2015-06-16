a9087309314d701d1b40afd43267b60977304b47
Daniel Manesajian <manesajian@gmail.com>
2015-06-15 22:58:33 -0700

    Initial add of binary tree implementation.

---
+++ binarytree.py
@@ -0,0 +1,23 @@
+class TreeNode():
+    def __init__(self):
+        self.prev = None
+        self.next = None
+
+class BinaryTree():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.__count = 0
+
+        # llist can iterate circularly
+        self.circular = circular
+
+    def __len__(self):
+        return self.__count
+
+    def depth_first_traversal():
+        pass
+
+    def breadth_first_traversal():
+        pass
+
