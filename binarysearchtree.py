6042963b7847461040f9e6e959607708af03dfef
Daniel Manesajian <manesajian@gmail.com>
2015-06-15 23:46:32 -0700

    Stubbed in basic interface.

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -1,3 +1,20 @@
 from binarytree import TreeNode, BinaryTree
 
-print('Test.')
+class BSTNode(TreeNode):
+    def __init__(self, key):
+        TreeNode.__init__(self)
+
+        self.key = key
+
+class BinarySearchTree(BinaryTree):
+    def __init__(self):
+        BinaryTree.__init__(self)
+
+    def insert_node(self, node):
+        pass
+
+    def find_node(self, node):
+        pass
+
+    def delete_node(self, node):
+        pass
