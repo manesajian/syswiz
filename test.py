34bf1dc854be1836c540b115af684ecf6cff938d
Daniel Manesajian <manesajian@gmail.com>
2015-06-25 16:20:33 -0700

    Beginning unittest use.

--- test.py
+++ test.py
@@ -1,17 +1,29 @@
+import unittest
+
 from binarysearchtree import BinarySearchTree
+from avltree import AVLTree
 
-bst = BinarySearchTree()
+class TestBinarySearchTree(unittest.TestCase):
 
-lst = [5, 8, 2, 1, 3, 9, 7, 4, 6, 0]
+    def setUp():
+        self.bst = BinarySearchTree()
 
-for i in lst:
-    bst.insert(i)
+    def tearDown():
+        pass
 
-for i in bst.root:
-    print(i)
+    def test_insert():
+        lst = [5, 8, 2, 1, 3, 9, 7, 4, 6, 0]
 
-bst.delete(5)
+        for i in lst:
+            self.bst.insert(i)
 
-for i in bst.root:
-    print(i)
+    def test_delete():
+        self.bst.delete(5)
 
+    def test_iteration():
+        lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for i in self.bst.root:
+            self.assertTrue(i == lst[i])
+
+if __name__ == '__main__':
+    unittest.main()
