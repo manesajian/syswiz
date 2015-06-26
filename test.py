271ab62c1a8342b919c1e074b6e8ad4812413a28
Daniel Manesajian <manesajian@gmail.com>
2015-06-25 20:06:29 -0700

    Fixed method definitions.

--- test.py
+++ test.py
@@ -4,23 +4,22 @@ from binarysearchtree import BinarySearchTree
 from avltree import AVLTree
 
 class TestBinarySearchTree(unittest.TestCase):
-
-    def setUp():
+    def setUp(self):
         self.bst = BinarySearchTree()
 
-    def tearDown():
+    def tearDown(self):
         pass
 
-    def test_insert():
+    def test_insert(self):
         lst = [5, 8, 2, 1, 3, 9, 7, 4, 6, 0]
 
         for i in lst:
             self.bst.insert(i)
 
-    def test_delete():
+    def test_delete(self):
         self.bst.delete(5)
 
-    def test_iteration():
+    def test_iteration(self):
         lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         for i in self.bst.root:
             self.assertTrue(i == lst[i])
