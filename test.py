e301aea181cbdbfa679c1e2068d482c0297e0c63
Daniel Manesajian <manesajian@gmail.com>
2015-06-26 23:57:15 -0700

    Added asserts to bst tests.

--- test.py
+++ test.py
@@ -7,22 +7,23 @@ class TestBinarySearchTree(unittest.TestCase):
     def setUp(self):
         self.bst = BinarySearchTree()
 
-    def tearDown(self):
-        pass
-
     def test_insert(self):
         lst = [5, 8, 2, 1, 3, 9, 7, 4, 6, 0]
-
         for i in lst:
             self.bst.insert(i)
+        self.assertEqual(len(self.bst), 10)
 
     def test_delete(self):
         self.bst.delete(5)
+        self.assertEqual(len(self.bst), 9)
 
     def test_iteration(self):
         lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         for i in self.bst.root:
             self.assertTrue(i == lst[i])
+
+    def tearDown(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
