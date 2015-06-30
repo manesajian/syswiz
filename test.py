4997f62d869a2900e66bfd04b1daecbd807efe35
Daniel Manesajian <manesajian@gmail.com>
2015-06-29 23:04:27 -0700

    Revised tests to be more self-contained.

--- test.py
+++ test.py
@@ -5,27 +5,37 @@ from avltree import AVLTree
 
 class TestBinarySearchTree(unittest.TestCase):
     def setUp(self):
-        print('setUp!')
-        self.bst = BinarySearchTree()
+        self.bst = None
 
-    def test_insert(self):
+    def _initialize(self):
+        self._cleanup()
+        self.bst = BinarySearchTree()
         lst = [5, 8, 2, 1, 3, 9, 7, 4, 6, 0]
         for i in lst:
             self.bst.insert(i)
+
+    def _cleanup(self):
+        if self.bst:
+            del(self.bst)
+            self.bst = None
+
+    def test_insert(self):
+        self._initialize()
         self.assertEqual(len(self.bst), 10)
 
     def test_delete(self):
+        self._initialize()
         self.bst.delete(5)
         self.assertEqual(len(self.bst), 9)
 
     def test_iteration(self):
+        self._initialize()
         lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         for i in self.bst.root:
             self.assertTrue(i == lst[i])
 
     def tearDown(self):
-        print('tearDown!')
-        pass
+        self._cleanup()
 
 if __name__ == '__main__':
     unittest.main()
