829954fad2e1269ff4a9e692f0d76d48f83373f8
Daniel Manesajian <manesajian@gmail.com>
2015-07-06 23:39:14 -0700

    New base class for trees.

---
+++ tree.py
@@ -0,0 +1,24 @@
+class TreeNode():
+    def __init__(self):
+        self.parent = None
+        self.value = None
+
+class Tree():
+    def __init__(self):
+        self.root = None
+        self.count = 0
+
+    def __len__(self):
+        return self.count
+
+    def height(self, node = None):
+        pass
+
+    def _height(self, node):
+        pass
+
+    def depth_first_traversal(self):
+        pass
+
+    def breadth_first_traversal(self):
+        pass
